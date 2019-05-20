77aa455af603cfaaf7ec3210fd83ae80d08cef68
Alex Berdyshev <berdishew@gmail.com>
2019-05-21 00:50:03 +0300

    Add bootstrap sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      includePaths: [
+        'node_modules/bootstrap-sass/assets/stylesheets'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +24,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('node_modules/bootstrap-sass/assets/javascripts/bootstrap.js');
 
   return app.toTree();
 };
