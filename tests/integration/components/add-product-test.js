94c4f9a34e4e7dc4b7e70f587a92ac121919a674
Alex Berdyshev <berdishew@gmail.com>
2019-05-21 18:04:51 +0300

    setup layout, rename add-product -> add-product-modal

--- tests/integration/components/add-product-test.js
+++ tests/integration/components/add-product-test.js
@@ -3,7 +3,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | add-product', function(hooks) {
+module('Integration | Component | add-product-modal', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
