6096ba03740fe32ec4038868a55fad7c16f37526
Alex Berdyshev <berdishew@gmail.com>
2019-05-22 14:52:13 +0300

    setup file uploading and rendering image in table

--- app/components/add-product-modal.js
+++ app/components/add-product-modal.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import Component from '@ember/component';
 import {computed, observer} from '@ember/object';
 
@@ -8,7 +9,7 @@ export default Component.extend({
   photo: '',
   isShowButtonVisible: false,
   isModalVisible: false,
-
+  firebaseApp: Ember.inject.service(),
   componentName: computed('action', function () {
     return `${this.action}Modal`;
   }),
@@ -23,11 +24,17 @@ export default Component.extend({
     this.showModal();
   }),
 
+  init(props) {
+    this._super(...arguments);
+    this.actions.uploadImage = this.actions.uploadImage.bind(this);
+  },
+
   didInsertElement() {
     if (this.isModalVisible) {
       this.showModal('show')
     }
   },
+
   showModal(action = 'toggle') {
     $(this.componentId).modal(action);
   },
@@ -63,6 +70,28 @@ export default Component.extend({
         throw Error(`Unknown @action name: ${this.action}`);
       }
       this.set('isModalVisible', !this.isModalVisible)
+    },
+
+    async uploadImage(file) {
+      const storage = await this.firebaseApp.storage();
+
+      this.set('isFileUploading', true);
+
+      const task = storage
+        .ref()
+        .child(`images/${file.name}`)
+        .put(file.blob, {
+          contentType: file.type
+        });
+
+      task
+        .then(snapshot => {
+          return snapshot.ref.getDownloadURL();
+        })
+        .then(url => {
+          this.set('isFileUploading', false);
+          this.set('photo', url);
+        })
     }
   }
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,3 +1,4 @@
+import EmberObject from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
@@ -6,5 +7,5 @@ export default Controller.extend({
     searchQuery: {
       refreshModel: true
     }
-  },
+  }
 });
