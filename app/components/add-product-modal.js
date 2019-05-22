d9569a593a34f9ff372dd7981515ad625cfdf639
Alex Berdyshev <berdishew@gmail.com>
2019-05-23 00:18:00 +0300

    Upload file logic refactoring

--- app/components/add-product-modal.js
+++ app/components/add-product-modal.js
@@ -7,15 +7,36 @@ export default Component.extend({
   title: '',
   description: '',
   photo: '',
+  fileToUpload: null,
+  fileToUploadBase64: null,
   isShowButtonVisible: false,
   isModalVisible: false,
+  isSaving: false,
   firebaseApp: Ember.inject.service(),
+
+  fileToUploadBase64Change: function () {
+    if (this.get('fileToUpload')) {
+      const reader = new FileReader();
+      reader.readAsDataURL(this.get('fileToUpload'));
+      reader.addEventListener("load", () => {
+        console.log('reader.result', reader.result)
+        this.set('fileToUploadBase64', reader.result)
+      }, false);
+    }
+
+    if (this.get('action') === 'update') {
+      this.set('fileToUploadBase64', this.get('photo'));
+    }
+  }.observes('photo', 'action', 'fileToUpload'),//.on('init'),
+
   componentName: computed('action', function () {
     return `${this.action}Modal`;
   }),
+
   componentId: computed('action', function () {
     return `#${this.componentName}`;
   }),
+
   modalTitle: computed('action', function () {
     return this.action === 'update' ? 'Update Product' : 'Add New Product';
   }),
@@ -23,11 +44,6 @@ export default Component.extend({
   visibilityChange: observer('isModalVisible', function () {
     this.showModal();
   }),
-
-  init(props) {
-    this._super(...arguments);
-    this.actions.uploadImage = this.actions.uploadImage.bind(this);
-  },
 
   didInsertElement() {
     if (this.isModalVisible) {
@@ -45,23 +61,50 @@ export default Component.extend({
       description: this.get('description'),
       photo: this.get('photo'),
     });
-    newProduct.save();
+    return newProduct.save();
   },
-  updateProduct() {
-    this.store
-      .findRecord('product', this.get('productId'), {backgroundReload: false})
-      .then((product) => {
-        product.setProperties({
-          title: this.get('title'),
-          description: this.get('description'),
-          photo: this.get('photo')
-        });
-        product.save()
+
+  async updateProduct() {
+    const product = await this.store
+      .findRecord('product', this.get('productId'), {backgroundReload: false});
+
+    product.setProperties({
+      title: this.get('title'),
+      description: this.get('description'),
+      photo: this.get('photo')
+    });
+
+    return product.save()
+  },
+
+  async uploadImage(file) {
+    const storage = await this.firebaseApp.storage();
+
+    this.set('isFileUploading', true);
+
+    const task = storage
+      .ref()
+      .child(`images/${file.name}`)
+      .put(file, {
+        contentType: file.type
       });
+
+    await task
+      .then(snapshot => snapshot.ref.getDownloadURL())
+      .then(url => {
+        this.set('isFileUploading', false);
+        this.set('photo', url);
+      })
   },
 
   actions: {
-    onSave() {
+    async onSave() {
+      this.set('isSaving', true);
+
+      if (this.get('fileToUpload')) {
+        await this.uploadImage(this.get('fileToUpload'))
+      }
+
       if (this.action === 'add') {
         this.createProduct();
       } else if (this.action === 'update') {
@@ -69,36 +112,23 @@ export default Component.extend({
       } else {
         throw Error(`Unknown @action name: ${this.action}`);
       }
-      this.set('isModalVisible', !this.isModalVisible)
+      this.set('isModalVisible', !this.isModalVisible);
+      this.set('isSaving', false);
     },
 
-    uploadImage(file) {
-      this.set('fileToUpload', file);
-      const reader = new FileReader();
+    onAddImage: function (file = []) {
+      this.set('fileToUpload', file[0]);
+      this.set('test', this.get('action'));
+      /*const reader = new FileReader();
       reader.readAsDataURL(file.blob);
-      reader.addEventListener("load", () => this.set('fileToUploadBase64', reader.result), false);
-    },
+      reader.addEventListener("load", () => this.set('fileToUploadBase64', reader.result), false);*/
+    },//.bind(this),
 
-    async uploadImageOrig(file) {
-      const storage = await this.firebaseApp.storage();
-
-      this.set('isFileUploading', true);
-
-      const task = storage
-        .ref()
-        .child(`images/${file.name}`)
-        .put(file.blob, {
-          contentType: file.type
-        });
-
-      task
-        .then(snapshot => {
-          return snapshot.ref.getDownloadURL();
-        })
-        .then(url => {
-          this.set('isFileUploading', false);
-          this.set('photo', url);
-        })
+    onRemoveImage() {
+      this.setProperties({
+        fileToUpload: null,
+        fileToUploadBase64: null
+      });
     }
   }
 });
